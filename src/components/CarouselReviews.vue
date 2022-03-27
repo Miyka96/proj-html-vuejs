c6f85a45ac7023c60e736c8fdd645a03f4da6499
Miyka96 <micaelaseminara@outlook.it>
2022-03-28 00:32:45 +0200

    carousel data

--- src/components/CarouselReviews.vue
+++ src/components/CarouselReviews.vue
@@ -20,11 +20,28 @@
             return{
                 carousel:[
                     {
-                        review:"",
-                        img:"",
-                        author:"",
-                        job:"",
-                        active: true,
+                        review:"I am free to learn at my own pace ,follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me",
+                        img: require(`../assets/img/testimonial-avata-02.jpg`),
+                        author:"Mina Hollace",
+                        job:"/Freelancer",
+                    },
+                                        {
+                        review:"I learned what expected to learn from this course. I love the presenters and the manner of delivery. Resources abound too. I'm purchasing this course. I hope to meet you, guys in persons. Thank you.",
+                        img: require(`../assets/img/testimonial-avata-01.jpg`),
+                        author:"Tom Anderson",
+                        job:"/Engineer",
+                    },
+                                        {
+                        review:"This course was designed with extremely outstanding qualities. The keen devotion of all the professors are highly appreciated. You have made it possible for me to move on in my professional career. ",
+                        img: require(`../assets/img/testimonial-avata-03.jpg`),
+                        author:"Patrick Weasle",
+                        job:"/Waiter",
+                    },
+                                        {
+                        review:"This course was very well structured; videos were short and to the point, while being informative and interesting to listen to. Thank you very much!",
+                        img: require(`../assets/img/testimonial-avata-04.jpg`),
+                        author:"Bill Craig",
+                        job:"/Freelancer",
                     },
                 ],
             }
